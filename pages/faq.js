8a52fa4410194e14e408d20e3f0adebaf890bc2e
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-03-01 11:56:32 -0300

    aula 0.8

---
+++ pages/faq.js
@@ -0,0 +1,9 @@
+export default function FAQPages() { 
+    return (
+        <div>
+
+            <h1>FAQ </h1>
+            <a href="/">Home</a>
+        </div>
+    )
+}
---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function HomePages() {
+    return (
+        <div>
+            <h1>Alura Cases - Home pages</h1>
+            <a href="/faq">alura FAQ</a>
+        </div>
+    )
+}
