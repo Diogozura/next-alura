0f7d28bd3ac3f0e18707d95af8417168d9197de4
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-03-02 12:02:48 -0300

    3,1

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import Link from "../src/componets/Link"
 
+
+
+function Title({ children, as }) {
+    const Tag = as
+    return (
+        <>
+            <Tag>{children}</Tag>
+
+            <style jsx>{`
+            ${Tag}{
+                color: red;
+                font-family: sans-serif;
+            }
+            `}
+
+            </style>
+        </>
+
+    )
+}
+
+
+
 export default function HomePages() {
     return (
         <div>
-            <h1>Alura Cases - Home pages</h1>
+            
+            <Title as="h2">Alura Cases - Home pages</Title>
             <Link href="/faq">
                 Ir para o FAQ
             </Link>
