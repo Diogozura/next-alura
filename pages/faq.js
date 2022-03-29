efd8b5452ef5c821c2e0a63c6b1880e34e45625b
Diogozura <65250673+Diogozura@users.noreply.github.com>
2022-03-29 17:32:20 -0300

    add pagina faq

--- pages/faq.js
+++ pages/faq.js
@@ -1,5 +1,4 @@
-import Head from "next/head"
-import Link from "../src/componets/Link"
+
 
 // SSG- Static Site Generation
 // SSR- Server Side Rendering
@@ -9,6 +8,11 @@ import Link from "../src/componets/Link"
 // export async function getServerSideProps() {
 //     console.log("em modo DEV, Sempre roda! A cada acesso")
 //     console.log("Rodando a cada acesso que você recebe")
+import FAQScreen from '../src/screens/FAQScreen';
+export default FAQScreen; 
+
+
+
 export async function getStaticProps() {
     //     console.log("em modo DEV, Sempre roda! A cada acesso")
     //     console.log("roda SOMENTE em build time")
@@ -29,37 +33,37 @@ export async function getStaticProps() {
     }
 }
 
-export default function FAQPages({ faq }) {
+// export default function FAQPages({ faq }) {
 
-    // console.log(faq)
-    // const [faq, setFaq] = useState([])
+//     // console.log(faq)
+//     // const [faq, setFaq] = useState([])
 
-    // useEffect(() => {
+//     // useEffect(() => {
 
-    // }, [])
+//     // }, [])
 
-    return (
-        <div>
-            <Head>
-                <title>FAQ</title>
-            </Head>
+//     return (
+//         <div>
+//             <Head>
+//                 <title>FAQ</title>
+//             </Head>
 
-            <h1>FAQ </h1>
-            <Link href="/">
-                Home
-            </Link>
-            <ul>
-                {faq.map(({ answer, question }) => (
-                    <li key={question}>
-                        <article>
-                            <h2>{question}</h2>
-                            <p>{answer}</p>
-                        </article>
+//             <h1>FAQ </h1>
+//             <Link href="/">
+//                 Home
+//             </Link>
+//             <ul>
+//                 {faq.map(({ answer, question }) => (
+//                     <li key={question}>
+//                         <article>
+//                             <h2>{question}</h2>
+//                             <p>{answer}</p>
+//                         </article>
 
-                    </li>
-                ))}
-            </ul>
+//                     </li>
+//                 ))}
+//             </ul>
 
-        </div>
-    )
-}
+//         </div>
+//     )
+// }
